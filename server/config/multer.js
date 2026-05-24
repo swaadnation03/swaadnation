3d4b40d23e2a8ae01cbc0edd123835af0b7d5e7d
Aditya Singh <adityasingh1464@gmail.com>
2026-05-24 14:23:40 +0530

    Add Cloudinary image upload support

---
+++ server/config/multer.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'swaadnation/products',
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp', 'gif'],
+    transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  }
+});
+
+const upload = multer({ 
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+
+module.exports = upload;
